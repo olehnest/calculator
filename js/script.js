3be9d0564b7688ffafaa5e5c561b80546b0a0310
olehnest <69309206+olehnest@users.noreply.github.com>
2020-08-30 12:35:33 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
-var btns = document.getElementsByTagName("button");
-            var calc = document.getElementById("calc");
+const display = document.querySelector('.display');
 
-            for (var thisBtn of btns) {
-                thisBtn.addEventListener('click', function() {
-                    if (this.innerHTML == "=")
-                        calc.value = eval(calc.value);
-                    else if (this.innerHTML == "C")
-                        calc.value = "";
-                    else
-                        calc.value += this.innerHTML;
-                })
-            }
+document.querySelectorAll('.digits button')
+.forEach( button => button.addEventListener('click', digitPressed));
+
+function digitPressed(ev) {
+   const digit = ev.target.innerText;
+   display.value += digit; 
+}
+
+document.querySelectorAll('.opers button')
+.forEach( button => button.addEventListener('click', operPressed));
+
+
+function operPressed(ev) {
+   const oper = ev.target.innerText;
+   display.value += oper;
+}
+
+document.querySelector('.eq').addEventListener('click', eqPressed);
+
+function eqPressed() {
+    display.value = eval(display.value);
+}
